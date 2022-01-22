b8b20870dade9a4c47c9a469bf07acb3eed4b5c8
Daniel Cañadillas <daniel.canadillas@outlook.com>
2022-01-22 17:13:34 +0100

    Merge

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,25 @@
 const { contextBridge, ipcRenderer, shell } = require("electron");
 const i18n = require("./i18n.js");
 
+contextBridge.exposeInMainWorld("api", {
+	send: (channel, data) => {
+		let validChannels = [
+			"inputDevicesAvailable",
+			"getInputDevice",
+			"saveSelectedInputDevice",
+		];
+		if (validChannels.includes(channel)) {
+			ipcRenderer.send(channel, data);
+		}
+	},
+	receive: (channel, func) => {
+		let validChannels = ["savedInputDevice"];
+		if (validChannels.includes(channel)) {
+			ipcRenderer.on(channel, (event, ...args) => func(...args));
+		}
+	},
+});
+
 contextBridge.exposeInMainWorld("electron", {
 	ipcRenderer: { ...ipcRenderer, on: ipcRenderer.on },
 	shell: { ...shell, openExternal: shell.openExternal },
--- src/views/js/renderUtils.js
+++ src/views/js/renderUtils.js
@@ -1,47 +1,59 @@
-async function loadConfiguration(){
-    loadInputDevicesList();
-
+async function loadConfiguration() {
+	window.i18n.changeLanguage(navigator.language);
+	loadInputDevicesList();
 }
 
-async function loadInputDevicesList(){
-    let config = getInputConfig();
-    let devices = getInputDevices();
-    let data = await Promise.all([config, devices]);
-    var devicesList = document.getElementById("inputDevicesList");
+async function loadInputDevicesList() {
+	console.log("Loading input devices list");
+	let config = await getInputConfig();
+	console.log("Input config:", config);
+	let devices = await getInputDevices();
+	console.log("Input devices:", devices);
+	let data = [config, devices];
+	console.log("Input data:", data);
+	var devicesList = document.getElementById("inputDevicesList");
+	console.log(data);
+	console.log(data[0]);
+	console.log(data[1]);
+	console.log(devicesList);
 
-    data[1].filter(device => device.kind ==="audioinput").forEach((device) =>{
-        let opt = createInputDevice(device.deviceId, device.label, data[0]==device.deviceId);
-        devicesList.appendChild(opt);
-    });
+	data[1]
+		.filter((device) => device.kind === "audioinput")
+		.forEach((device) => {
+			let opt = createInputDevice(
+				device.deviceId,
+				device.label,
+				data[0] == device.deviceId
+			);
+			devicesList.appendChild(opt);
+		});
 
-    // remove loading... option and setting the list able
-    devicesList.options[0].remove();
-    devicesList.removeAttribute("disabled");
-
-    
-
+	// remove loading... option and setting the list able
+	devicesList.options[0].remove();
+	devicesList.removeAttribute("disabled");
 }
-async function getInputConfig(){
-    return new Promise(resolve=> {
-        window.api.send("getInputDevice", null);
-        window.api.receive("savedInputDevice", (data) => {
-            resolve(data);
-    })});
+async function getInputConfig() {
+	return new Promise((resolve) => {
+		window.api.send("getInputDevice", null);
+		window.api.receive("savedInputDevice", (data) => {
+			resolve(data);
+		});
+	});
 }
 async function getInputDevices() {
-    return Promise.resolve(navigator.mediaDevices.enumerateDevices());
+	return Promise.resolve(navigator.mediaDevices.enumerateDevices());
 }
 
-function createInputDevice(id, name, selected=false){
-    let opt = document.createElement("option");
-    opt.setAttribute("id", id);
-    opt.innerText= name;
+function createInputDevice(id, name, selected = false) {
+	let opt = document.createElement("option");
+	opt.setAttribute("id", id);
+	opt.innerText = name;
 
-    if(selected) opt.setAttribute("selected", "selected")
-    return opt;
+	if (selected) opt.setAttribute("selected", "selected");
+	return opt;
 }
 
-function saveInputDeviceConfig(select){
-    let id = select[select.selectedIndex].id;
-    window.api.send("saveSelectedInputDevice", id);
+function saveInputDeviceConfig(select) {
+	let id = select[select.selectedIndex].id;
+	window.api.send("saveSelectedInputDevice", id);
 }
